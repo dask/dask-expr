d7218fb6fcc7cf1ce163491ada32c68b4fd0ae77
Matthew Rocklin <mrocklin@gmail.com>
2023-03-29 09:15:24 -0500

    Separate Parquet tests from collection tests

--- dask_match/tests/test_collection.py
+++ dask_match/tests/test_collection.py
@@ -1,40 +1,21 @@
-import os
-
 import pandas as pd
 import pytest
 from dask.dataframe.utils import assert_eq
 from dask.utils import M
 
-from dask_match import from_pandas, optimize, read_csv, read_parquet
+from dask_match import from_pandas, optimize
 
 
-def _make_file(dir, format="parquet", df=None):
-    fn = os.path.join(str(dir), f"myfile.{format}")
-    if df is None:
-        df = pd.DataFrame({c: range(10) for c in "abcde"})
-    if format == "csv":
-        df.to_csv(fn)
-    elif format == "parquet":
-        df.to_parquet(fn)
-    else:
-        ValueError(f"{format} not a supported format")
-    return fn
+@pytest.fixture
+def df():
+    df = pd.DataFrame({"x": range(100)})
+    df["y"] = df.x * 10.0
+    yield df
 
 
-def test_basic(tmpdir):
-    fn_pq = _make_file(tmpdir, format="parquet")
-    fn_csv = _make_file(tmpdir, format="csv")
-
-    x = read_parquet(fn_pq, columns=("a", "b", "c"))
-    y = read_csv(fn_csv, usecols=("a", "d", "e"))
-
-    z = x + y
-    result = z[["a", "b", "d"]].sum(skipna=False)
-    assert result.operand("skipna") is False
-    assert list(result.operands[0].columns) == ["a", "b", "d"]
-
-    x + 1
-    1 + x
+@pytest.fixture
+def ddf(df):
+    yield from_pandas(df, npartitions=10)
 
 
 def test_del():
@@ -57,50 +38,6 @@ def test_setitem():
 
     assert "z" in ddf.columns
     assert_eq(ddf, ddf)
-
-
-def df(fn):
-    return read_parquet(fn, columns=["a", "b", "c"])
-
-
-def df_bc(fn):
-    return read_parquet(fn, columns=["b", "c"])
-
-
-@pytest.mark.parametrize(
-    "input,expected",
-    [
-        (
-            # Add -> Mul
-            lambda fn: df(fn) + df(fn),
-            lambda fn: 2 * df(fn),
-        ),
-        (
-            # Column projection
-            lambda fn: df(fn)[["b", "c"]],
-            lambda fn: read_parquet(fn, columns=["b", "c"]),
-        ),
-        (
-            # Compound
-            lambda fn: 3 * (df(fn) + df(fn))[["b", "c"]],
-            lambda fn: 6 * df_bc(fn),
-        ),
-        (
-            # Traverse Sum
-            lambda fn: df(fn).sum()[["b", "c"]],
-            lambda fn: df_bc(fn).sum(),
-        ),
-        (
-            # Respect Sum keywords
-            lambda fn: df(fn).sum(numeric_only=True)[["b", "c"]],
-            lambda fn: df_bc(fn).sum(numeric_only=True),
-        ),
-    ],
-)
-def test_optimize(tmpdir, input, expected):
-    fn = _make_file(tmpdir, format="parquet")
-    result = optimize(input(fn))
-    assert str(result.expr) == str(expected(fn).expr)
 
 
 def test_meta_divisions_name():
@@ -127,15 +64,11 @@ def test_meta_blockwise():
     assert set(cc.columns) == {"x", "y", "z"}
 
 
-def test_dask():
-    df = pd.DataFrame({"x": range(100), "y": range(100)})
-    df["y"] = df.y * 10.0
-
-    ddf = from_pandas(df, npartitions=10)
+def test_dask(df, ddf):
     assert (ddf.x + ddf.y).npartitions == 10
     z = (ddf.x + ddf.y).sum()
 
-    assert z.compute() == (df.x + df.y).sum()
+    assert assert_eq(z, (df.x + df.y).sum())
 
 
 @pytest.mark.parametrize(
@@ -152,13 +85,9 @@ def test_dask():
         ),
     ],
 )
-def test_reductions(func):
-    df = pd.DataFrame({"x": range(100), "y": range(100)})
-    df["y"] = df.y * 10.0
-    ddf = from_pandas(df, npartitions=10)
-
+def test_reductions(func, df, ddf):
     assert_eq(func(ddf), func(df))
-    assert func(ddf.x).compute() == func(df.x)
+    assert_eq(func(ddf.x), func(df.x))
 
 
 def test_mode():
@@ -182,45 +111,8 @@ def test_mode():
         lambda df: df.x != df.y,
     ],
 )
-def test_conditionals(func):
-    df = pd.DataFrame({"x": range(100), "y": range(100)})
-    df["y"] = df.y * 2.0
-    ddf = from_pandas(df, npartitions=10)
-
+def test_conditionals(func, df, ddf):
     assert_eq(func(df), func(ddf), check_names=False)
-
-
-def test_predicate_pushdown(tmpdir):
-    from dask_match.io import ReadParquet
-
-    original = pd.DataFrame(
-        {
-            "a": [1, 2, 3, 4, 5] * 10,
-            "b": [0, 1, 2, 3, 4] * 10,
-            "c": range(50),
-            "d": [6, 7] * 25,
-            "e": [8, 9] * 25,
-        }
-    )
-    fn = _make_file(tmpdir, format="parquet", df=original)
-    df = read_parquet(fn)
-    assert_eq(df, original)
-    x = df[df.a == 5][df.c > 20]["b"]
-    y = optimize(x)
-    assert isinstance(y.expr, ReadParquet)
-    assert ("a", "==", 5) in y.expr.operand("filters") or (
-        "a",
-        "==",
-        5,
-    ) in y.expr.operand("filters")
-    assert ("c", ">", 20) in y.expr.operand("filters")
-    assert list(y.columns) == ["b"]
-
-    # Check computed result
-    y_result = y.compute()
-    assert list(y_result.columns) == ["b"]
-    assert len(y_result["b"]) == 6
-    assert all(y_result["b"] == 4)
 
 
 @pytest.mark.parametrize(
@@ -232,41 +124,28 @@ def test_predicate_pushdown(tmpdir):
         lambda df: df.assign(a=df.x + df.y, b=df.x - df.y),
     ],
 )
-def test_blockwise(func):
-    df = pd.DataFrame({"x": range(20), "y": range(20)})
-    df["y"] = df.y * 2.0
-    ddf = from_pandas(df, npartitions=3)
-
+def test_blockwise(func, df, ddf):
     assert_eq(func(df), func(ddf))
 
 
-def test_repr():
-    df = pd.DataFrame({"x": range(20), "y": range(20)})
-    df = from_pandas(df, npartitions=1)
+def test_repr(ddf):
+    assert "+ 1" in str(ddf + 1)
+    assert "+ 1" in repr(ddf + 1)
 
-    assert "+ 1" in str(df + 1)
-    assert "+ 1" in repr(df + 1)
-
-    s = (df["x"] + 1).sum(skipna=False).expr
+    s = (ddf["x"] + 1).sum(skipna=False).expr
     assert '["x"]' in s or "['x']" in s
     assert "+ 1" in s
     assert "sum(skipna=False)" in s
 
 
-def test_columns_traverse_filters():
-    df = pd.DataFrame({"x": range(20), "y": range(20), "z": range(20)})
-    df = from_pandas(df, npartitions=2)
-
-    result = optimize(df[df.x > 5].y)
-    expected = df.y[df.x > 5]
+def test_columns_traverse_filters(df, ddf):
+    result = optimize(ddf[ddf.x > 5].y)
+    expected = ddf.y[ddf.x > 5]
 
     assert str(result) == str(expected)
 
 
-def test_persist():
-    df = pd.DataFrame({"x": range(20), "y": range(20), "z": range(20)})
-    ddf = from_pandas(df, npartitions=2)
-
+def test_persist(df, ddf):
     a = ddf + 2
     b = a.persist()
 
@@ -275,4 +154,4 @@ def test_persist():
 
     assert len(b.__dask_graph__()) == b.npartitions
 
-    assert b.y.sum().compute() == (df + 2).y.sum()
+    assert_eq(b.y.sum(), (df + 2).y.sum())
