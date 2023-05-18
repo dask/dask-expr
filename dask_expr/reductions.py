1e357e0c92b3f4fa0508fc1b4819ef6a7251c264
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-05-18 10:56:32 +0200

    Remove numeric_only for now

--- dask_expr/reductions.py
+++ dask_expr/reductions.py
@@ -251,7 +251,8 @@ class IdxMin(Reduction):
 
     @property
     def chunk_kwargs(self):
-        return dict(skipna=self.skipna, numeric_only=self.numeric_only, fn=self._fn)
+        # TODO: Add numeric_only after Dask release on May 26th
+        return dict(skipna=self.skipna, fn=self._fn)
 
     @property
     def combine_kwargs(self):
