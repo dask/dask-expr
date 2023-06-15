072e2dcb0f304c969d772bd0ddd7109e95416cd2
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-06-15 12:46:39 +0200

    Add more tests

--- dask_expr/tests/test_concat.py
+++ dask_expr/tests/test_concat.py
@@ -25,10 +25,37 @@ def test_concat(pdf, df):
     assert all(div is None for div in result.divisions)
 
 
+def test_concat_pdf(pdf, df):
+    result = concat([df, pdf])
+    expected = pd.concat([pdf, pdf])
+    assert_eq(result, expected)
+    assert all(div is None for div in result.divisions)
+
+
 def test_concat_divisions(pdf, df):
     pdf2 = pdf.set_index(np.arange(200, 300))
     df2 = from_pandas(pdf2, npartitions=10)
     result = concat([df, df2])
     expected = pd.concat([pdf, pdf2])
+    assert_eq(result, expected)
+    assert not any(div is None for div in result.divisions)
+
+
+@pytest.mark.parametrize("join", ["right", "left"])
+def test_invalid_joins(join):
+    with pytest.raises(ValueError, match="'join' must be"):
+        concat([df, df], join=join)
+
+
+def test_concat_invalid():
+    with pytest.raises(TypeError, match="dfs must"):
+        concat(df)
+    with pytest.raises(ValueError, match="No objects to"):
+        concat([])
+
+
+def test_concat_one_object(df, pdf):
+    result = concat([df])
+    expected = pd.concat([pdf])
     assert_eq(result, expected)
     assert not any(div is None for div in result.divisions)
