80a4b5dad8d986b6e2acd4f659d1966984fecda0
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-06-26 15:43:38 +0200

    Add utility to convert to list

--- dask_expr/_util.py
+++ dask_expr/_util.py
@@ -1,8 +1,10 @@
-def _maybe_convert_to_list(column):
-    if (
-        column is not None
-        and not isinstance(column, list)
-        and not hasattr(column, "dtype")
-    ):
+def _convert_to_list(column):
+    if column is None or isinstance(column, list):
+        pass
+    elif isinstance(column, tuple):
+        column = list(column)
+    elif hasattr(column, "dtype"):
+        column = column.tolist()
+    else:
         column = [column]
     return column
