8813a0b6ec5ad2cf2e636791a0e19518ade87b46
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-07-25 19:56:35 +0200

    Update dask_expr/tests/test_concat.py
    Co-authored-by: Richard (Rick) Zamora <rzamora217@gmail.com>

--- dask_expr/tests/test_concat.py
+++ dask_expr/tests/test_concat.py
@@ -103,7 +103,7 @@ def test_concat_axis_one_co_aligned(pdf, df):
     assert_eq(concat([df, df2], axis=1), pd.concat([pdf, pdf2], axis=1))
 
 
-def test_concat_axis_one_all_divisions_unknown(pdf, df):
+def test_concat_axis_one_all_divisions_unknown(pdf):
     pdf = pdf.sort_values(by="x", ascending=False, ignore_index=True)
     df = from_pandas(pdf, npartitions=2, sort=False)
     pdf2 = pdf.add_suffix("_2")
