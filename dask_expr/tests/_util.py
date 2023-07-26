42ecf35ee9bd11c2ad4775bce48b6950947bc802
rjzamora <rzamora217@gmail.com>
2023-07-26 11:17:34 -0700

    simplify xfail_gpu

--- dask_expr/tests/_util.py
+++ dask_expr/tests/_util.py
@@ -12,9 +12,7 @@ def _backend_library():
     return importlib.import_module(_backend_name())
 
 
-def xfail_gpu(reason=None, skip=False):
+def xfail_gpu(reason=None):
     condition = _backend_name() == "cudf"
     reason = reason or "Failure expected for cudf backend."
-    if not skip:
-        return pytest.mark.xfail(condition, reason=reason)
-    return pytest.mark.skipif(condition, reason=reason)
+    return pytest.mark.xfail(condition, reason=reason)
