e165acd7b12da0dcc7c0ca949715cfa23630de69
Rick Zamora <rzamora217@gmail.com>
2023-08-17 09:01:25 -0500

    improve testing

--- dask_expr/tests/test_datasets.py
+++ dask_expr/tests/test_datasets.py
@@ -95,11 +95,20 @@ def test_combine_similar(tmpdir):
 
 @pytest.mark.parametrize("seed", [42, None])
 def test_timeseries_deterministic_head(seed):
+    # Make sure our `random_state` code gives
+    # us deterministic results
     df = timeseries(end="2000-01-02", seed=seed)
     assert_eq(df.head(), df.head())
+    assert_eq(df["x"].head(), df.head()["x"])
+    assert_eq(df.head()["x"], df["x"].partitions[0].compute().head())
 
 
 def test_timeseries_gaph_size():
+    # Check that our graph size is reasonable
     df = timeseries(end="2000-01-03", seed=42)
     graph_size = sys.getsizeof(pickle.dumps(df.dask))
+    # This criteria is somewhat arbitrary. The
+    # original `random_state` code was producing
+    # ~5KB here. So, we know the size should
+    # always remain smaller than that.
     assert graph_size < 1024
