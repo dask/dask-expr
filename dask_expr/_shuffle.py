0534c99c01c5dcbf5f3ea3814ff84ca2635c11e3
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-09-22 17:20:31 +0100

    Fix test

--- dask_expr/_shuffle.py
+++ dask_expr/_shuffle.py
@@ -779,7 +779,7 @@ class SetIndex(BaseSetIndexSortValues):
             if any(isinstance(x, Index) for x in p.walk()):
                 # Punt on cases where the new index is part of the filter
                 return
-            predicate = parent.substitute({self: self.frame}).predicate
+            predicate = parent.substitute(self, self.frame).predicate
             return type(self)(self.frame[predicate], *self.operands[1:])
 
 
