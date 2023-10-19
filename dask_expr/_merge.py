ba322d7ac7a6dcaf2e8b4369ebce215ff3686fa8
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-10-19 11:07:37 +0200

    Get rid of SettingWithCopyWarnings

--- dask_expr/_merge.py
+++ dask_expr/_merge.py
@@ -513,8 +513,8 @@ def create_assign_index_merge_transfer():
             index = partitioning_index(df[index], npartitions)
         else:
             index = partitioning_index(index, npartitions)
-        df[name] = index
-        meta[name] = 0
+        df = df.assign(**{name: index})
+        meta = meta.assign(**{name: 0})
         return merge_transfer(df, id, input_partition, npartitions, meta, parts_out)
 
     return assign_index_merge_transfer
