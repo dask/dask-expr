a191ddbf848da90e7f0294817adb1f521dc9262c
rjzamora <rzamora217@gmail.com>
2023-10-23 11:40:37 -0700

    allow pandas=1.5.3 but warn on import

--- dask_expr/__init__.py
+++ dask_expr/__init__.py
@@ -2,3 +2,12 @@ from dask_expr import _version, datasets
 from dask_expr._collection import *
 
 __version__ = _version.get_versions()["version"]
+
+from dask.dataframe._compat import PANDAS_GE_200
+
+if not PANDAS_GE_200:
+    warnings.warn(
+        "The installed Pandas version is not recommended for "
+        "`dask_expr`. Please update to `pandas>=2` if possible."
+        "\nRAPIDS users can ignore this warning."
+    )
