60f67cc64f96fe7ff120876a057bf5a9e67a6667
crusaderky <crusaderky@gmail.com>
2023-12-11 16:12:43 +0000

    cosmetic

--- dask_expr/tests/test_collection.py
+++ dask_expr/tests/test_collection.py
@@ -1657,14 +1657,15 @@ def test_map_overlap():
     assert_eq(result, expected, check_index=False)
 
 
-def test_quantile(df, pdf):
+def test_quantile(df):
+    assert_eq(df.x.quantile(), 49.0)
     assert_eq(
         df.x.quantile(q=[0.2, 0.8]),
         lib.Series([19.0, 79.0], index=[0.2, 0.8], name="x"),
     )
-    assert_eq(df.x.quantile(), 49.0)
     assert_eq(
-        df.x.index.quantile(q=[0.2, 0.8]), lib.Series([19.0, 79.0], index=[0.2, 0.8])
+        df.x.index.quantile(q=[0.2, 0.8]),
+        lib.Series([19.0, 79.0], index=[0.2, 0.8]),
     )
 
 
