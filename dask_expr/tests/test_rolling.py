707cedb69ab27aa8eaafb648dfcfd02c804a6d35
Miles Granger <miles59923@gmail.com>
2023-12-11 08:30:33 +0100

    Remove axis keyword tests, pandas raises futurewarning

--- dask_expr/tests/test_rolling.py
+++ dask_expr/tests/test_rolling.py
@@ -147,8 +147,6 @@ def test_rolling_raises():
     pytest.raises(ValueError, lambda: ddf.rolling(-1))
     pytest.raises(ValueError, lambda: ddf.rolling(3, min_periods=1.2))
     pytest.raises(ValueError, lambda: ddf.rolling(3, min_periods=-2))
-    pytest.raises(ValueError, lambda: ddf.rolling(3, axis=10))
-    pytest.raises(ValueError, lambda: ddf.rolling(3, axis="coulombs"))
     pytest.raises(NotImplementedError, lambda: ddf.rolling(100).mean().compute())
 
 
