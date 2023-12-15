7ae2c02315e60f90e9cbb69f800dd48ab65ec4cf
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-12-15 18:20:00 +0100

    Adjust test

--- dask_expr/tests/test_groupby.py
+++ dask_expr/tests/test_groupby.py
@@ -161,18 +161,11 @@ def test_groupby_agg_grouper_single(pdf):
     df = from_pandas(pdf, npartitions=2)
 
     result = df.groupby("x")["x"].agg(["min", "max"])
-    print(result.compute())
     expected = pdf.groupby("x")["x"].agg(["min", "max"])
     assert_eq(result, expected)
 
-
-@pytest.mark.parametrize("slice_", ["a", ["a"], ["a", "b"], ["b"]])
-def test_groupby_agg_grouper_multiple(slice_):
-    d = lib.DataFrame({"a": [1, 2, 3, 4], "b": [1, 2, 3, 4]})
-    a = from_pandas(d, npartitions=2)
-
-    result = a.groupby("a")[slice_].agg(["min", "max"])
-    expected = d.groupby("a")[slice_].agg(["min", "max"])
+    result = df.groupby("x")[["x"]].agg(["min", "max"])
+    expected = pdf.groupby("x")[["x"]].agg(["min", "max"])
     assert_eq(result, expected)
 
 
