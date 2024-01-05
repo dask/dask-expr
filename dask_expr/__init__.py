ee359fb4d9e3c6d4264894e71bb61b69024398a6
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2024-01-05 23:59:33 +0100

    Add read and to json

--- dask_expr/__init__.py
+++ dask_expr/__init__.py
@@ -1,6 +1,7 @@
 from dask_expr import _version, datasets
 from dask_expr._collection import *
 from dask_expr.io._delayed import from_delayed
+from dask_expr.io.json import read_json, to_json
 from dask_expr.io.parquet import to_parquet
 
 __version__ = _version.get_versions()["version"]
