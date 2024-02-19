b5c6e9a0a6d924a630c21f167ee340e37a7fa15e
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2024-02-19 15:33:02 +0100

    Update

--- dask_expr/_expr.py
+++ dask_expr/_expr.py
@@ -3485,16 +3485,19 @@ def determine_column_projection(expr, parent, dependents, additional_columns=Non
 
 def _sort_mixed(values):
     """order ints before strings before nulls in 1d arrays"""
-    str_pos = np.array([isinstance(x, (str, tuple)) for x in values], dtype=bool)
+    str_pos = np.array([isinstance(x, str) for x in values], dtype=bool)
+    tuple_pos = np.array([isinstance(x, str) for x in values], dtype=bool)
     null_pos = np.array([pd.isna(x) for x in values], dtype=bool)
-    num_pos = ~str_pos & ~null_pos
+    num_pos = ~str_pos & ~null_pos & ~tuple_pos
     str_argsort = np.argsort(values[str_pos])
+    tuple_argsort = np.argsort(values[tuple_pos])
     num_argsort = np.argsort(values[num_pos])
     # convert boolean arrays to positional indices, then order by underlying values
     str_locs = str_pos.nonzero()[0].take(str_argsort)
+    tuple_locs = str_pos.nonzero()[0].take(tuple_argsort)
     num_locs = num_pos.nonzero()[0].take(num_argsort)
     null_locs = null_pos.nonzero()[0]
-    locs = np.concatenate([num_locs, str_locs, null_locs])
+    locs = np.concatenate([num_locs, str_locs, tuple_locs, null_locs])
     return values.take(locs)
 
 
