8ce4c6dc3ba5cf21a1f6cd2c0e2c63ae1a0d121e
rjzamora <rzamora217@gmail.com>
2024-05-01 11:05:48 -0700

    add melt support

--- dask_expr/_collection.py
+++ dask_expr/_collection.py
@@ -3988,6 +3988,35 @@ class DataFrame(FrameBase):
             layers=maybe_pluralize(n_expr, "expression"),
         )
 
+    @derived_from(pd.DataFrame)
+    def melt(
+        self,
+        id_vars=None,
+        value_vars=None,
+        var_name=None,
+        value_name="value",
+        col_level=None,
+    ):
+        meta = make_meta(
+            meta_nonempty(self._meta).melt(
+                id_vars=id_vars,
+                value_vars=value_vars,
+                var_name=var_name,
+                value_name=value_name,
+                col_level=col_level,
+            )
+        )
+        return self.map_partitions(
+            M.melt,
+            clear_divisions=True,
+            meta=meta,
+            id_vars=id_vars,
+            value_vars=value_vars,
+            var_name=var_name,
+            value_name=value_name,
+            col_level=col_level,
+        )
+
     def _repr_data(self):
         meta = self._meta
         index = self._repr_divisions
