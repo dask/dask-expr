96e2681bdd991b9a7a1fb78199bbb7b6968a721b
rjzamora <rzamora217@gmail.com>
2024-07-31 13:37:44 -0700

    add test coverage

--- dask_expr/_backends.py
+++ dask_expr/_backends.py
@@ -5,7 +5,6 @@ import pandas as pd
 from dask.backends import CreationDispatch
 from dask.dataframe.backends import DataFrameBackendEntrypoint
 
-from dask_expr._collection import new_collection
 from dask_expr._dispatch import get_collection_type
 from dask_expr._expr import ToBackend
 
@@ -41,6 +40,11 @@ class ToPandasBackend(ToBackend):
 
         return to_pandas_dispatch(df, **options)
 
+    def _simplify_down(self):
+        if isinstance(self.frame._meta, (pd.DataFrame, pd.Series, pd.Index)):
+            # We already have pandas data
+            return self.frame
+
 
 class PandasBackendEntrypoint(DataFrameBackendEntrypoint):
     """Pandas-Backend Entrypoint Class for Dask-Expressions
@@ -50,19 +54,10 @@ class PandasBackendEntrypoint(DataFrameBackendEntrypoint):
     """
 
     @classmethod
-    def to_backend_dispatch(cls):
-        from dask.dataframe.dispatch import to_pandas_dispatch
-
-        return to_pandas_dispatch
-
-    @classmethod
     def to_backend(cls, data, **kwargs):
-        if isinstance(data._meta, (pd.DataFrame, pd.Series, pd.Index)):
-            # Already a pandas-backed collection
-            return data
-        return new_collection(
-            ToPandasBackend(data, kwargs)
-        )  # data.map_partitions(cls.to_backend_dispatch(), **kwargs)
+        from dask_expr._collection import new_collection
+
+        return new_collection(ToPandasBackend(data, kwargs))
 
 
 dataframe_creation_dispatch.register_backend("pandas", PandasBackendEntrypoint())
